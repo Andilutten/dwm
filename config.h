c21d67f6347c02d3ed3962dc47eab2476c0efdcc
Andreas Malmqvist <andreasmalmqvist1992@gmail.com>
2019-03-12 17:58:06 +0100

    yellow window border

--- config.h
+++ config.h
@@ -18,8 +18,10 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_yellow[]        = "#d8af5f";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
+	/* [SchemeNorm] = { col_gray3, col_gray1, col_gray2 }, */
+	/* [SchemeSel]  = { col_gray4, col_yellow,  col_yellow  }, */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_yellow,  col_yellow  },
+	[SchemeSel]  = { col_gray4, col_gray1,  col_yellow  },
 };
 
 /* tagging */
